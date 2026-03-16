959314071417704e688e847fafee13b2757381eb
bennajah <bennajah.elmahdi@gmail.com>
2026-03-16 03:21:44 +0000

    init

---
+++ src/lib/server/auth.ts
@@ -0,0 +1,15 @@
+import { getRequestEvent } from '$app/server';
+import { env } from '$env/dynamic/private';
+import { db } from '$lib/server/db';
+import * as schema from '$lib/server/db/schema';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { betterAuth } from 'better-auth/minimal';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+
+export const auth = betterAuth({
+	baseURL: env.ORIGIN,
+	secret: env.BETTER_AUTH_SECRET,
+	database: drizzleAdapter(db, { provider: 'pg', schema, usePlural: true }),
+	emailAndPassword: { enabled: true },
+	plugins: [sveltekitCookies(getRequestEvent)]
+});
